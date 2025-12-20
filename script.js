b5d066d21236caf900e021863c1e73b44548f716
Lahiru Madhusanka <lahirumadushanka82887721@gmail.com>
2025-12-20 12:34:46 +0530

    4th commi

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let name = "Lahiru Avishka Madhusanka";
-let email = "lahhiru@gmail.com";
+let email = "lahiru@gmail.com";
 let age = 23;
 
 var output = `My name is ${name} and I am ${age} years old.`;
